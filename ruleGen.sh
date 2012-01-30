07e488f03828a557babb26c1bdadfa079f4033ac
phi <phi@phi.(none)>
2012-01-29 19:56:08 -0500

    updated cdh3 version, works in standalone mode for latest release

--- ruleGen.sh
+++ ruleGen.sh
@@ -5,10 +5,10 @@ hadoop fs -rmr outputdir2
 rm results.txt
 
 INPUT=testL/testLong.txt
+JAR=/usr/lib/hadoop/contrib/streaming/hadoop-streaming-0.20.2-cdh3u2.jar
+hadoop jar $JAR -input $INPUT -output outputdir -mapper fpMap.py -reducer fpReduce.py -file fpReduce.py -file fpMap.py  
 
-hadoop jar /usr/lib/hadoop/contrib/streaming/hadoop-streaming-0.20.2-cdh3u0.jar -input $INPUT -output outputdir -mapper fpMap.py -reducer fpReduce.py -file fpReduce.py -file fpMap.py  
 
-
-hadoop jar /usr/lib/hadoop/contrib/streaming/hadoop-streaming-0.20.2-cdh3u0.jar -input outputdir/p* -output outputdir2 -mapper ruleMap.py -reducer ruleReduce.py -file ruleReduce.py -file ruleMap.py  
+hadoop jar $JAR -input outputdir/p* -output outputdir2 -mapper ruleMap.py -reducer ruleReduce.py -file ruleReduce.py -file ruleMap.py  
 
 hadoop fs -cat outputdir2/part* > results.txt
